a554a0a1b890ea9e27c60ee3ca5934e80e7c62b5
Jean Park <kestry@gmail.com>
2017-04-16 14:08:18 -0700

    working tests, createFile, destroyFile. use 'make test' and then 'p1test' to see test

--- proj1/codebase/rbf/p1test.cc
+++ proj1/codebase/rbf/p1test.cc
@@ -1,3 +1,5 @@
+//p1test.cc -- unit tests for project 1
+
 #include <iostream>
 #include <string>
 #include <cassert>
@@ -18,14 +20,19 @@ int main() {
    string filename = "00_exist.t";
    const char* cfname = filename.c_str(); 
    int rc = pfm->createFile (cfname);
-   printf("test_00: rc for creating filename %s is: %d.\n", 
+   printf("test_00: creatFile(%s) returned: %d.\n", 
           cfname, rc);
 
    filename = "01_create.t";
    cfname = filename.c_str();
    rc = pfm->createFile (cfname);
-   printf("test_01: rc for creating filename %s is: %d.\n", cfname, rc);
+   printf("test_01: createFile(%s) returned: %d.\n", cfname, rc);
    
+   filename = "02_destroy.t";
+   cfname = filename.c_str();
+   rc = pfm->destroyFile (cfname);
+   printf("test_01: destroyFile(%s) returned: %d.\n", cfname, rc);
+
    cout << "done" << endl;
    return 0;
 }
